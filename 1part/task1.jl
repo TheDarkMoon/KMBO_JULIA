2a7a5c0dc5fab4dc3461302447fc3980ddd8194a
TheDarkMoon <75882411+TheDarkMoon@users.noreply.github.com>
2020-12-15 02:17:26 +0300

    Update task1.jl

--- 1part/task1.jl
+++ 1part/task1.jl
@@ -1,10 +1,8 @@
 function inverse(side::HorizonSide)
-    """Inverse the side"""
     return HorizonSide(mod(Int(side)+2, 4)) 
 end
 
 function main(r::Robot)
-    """Main function"""
     for side in sides
         putmarkers!(r,side)
         followMarkers(r,inverse(side))
@@ -13,7 +11,6 @@ function main(r::Robot)
 end
 
 function putmarkers!(r::Robot,side::HorizonSide)
-    """As long as robot is able to move put marker"""
     while isborder(r,side)==false 
         move!(r,side)
         putmarker!(r)
@@ -21,7 +18,6 @@ function putmarkers!(r::Robot,side::HorizonSide)
 end
 
 function followMarkers(r::Robot,side::HorizonSide)
-    """Move in the direction as long as robot is on marker"""
     while ismarker(r)==true 
         move!(r,side) 
     end
